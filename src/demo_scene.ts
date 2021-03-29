d13e67a8511621ca44c793533ef269c64bc02120
doongjohn <doongjohn@gmail.com>
2021-03-29 19:36:26 +0900

    update

--- src/demo_scene.ts
+++ src/demo_scene.ts
@@ -1,5 +1,6 @@
 type Scene = Phaser.Scene;
 type GameObject = Phaser.GameObjects.GameObject;
+type Rectangle = Phaser.GameObjects.Rectangle;
 type Vector2 = Phaser.Math.Vector2;
 const Vector2 = Phaser.Math.Vector2;
 
@@ -13,45 +14,34 @@ export default class DemoScene extends Phaser.Scene {
 	}
 
 	create() {
-		const mainCam = this.cameras.main;
-		const center = new Vector2(mainCam.centerX, mainCam.centerY);
-		const world = this.add.container(center.x, center.y);
+		const camMain = this.cameras.main;
+		const camCenter = () => new Vector2(camMain.centerX, camMain.centerY);
+		const world = this.add.container(camCenter().x, camCenter().y);
 
-		const gridSize = new Vector2(3, 3);
-		const cellSize = new Vector2(150, 150);
-		const tileSize = new Vector2(130, 130);
-		const gapSize = new Vector2(
-			cellSize.x - tileSize.x,
-			cellSize.y - tileSize.y
-		);
+		// Grid settings
+		const gridSize = new Vector2(8, 8);
+		const gapSize = new Vector2(10, 10);
+		const tileSize = new Vector2(100, 100);
+		const cellSize = tileSize.clone().add(gapSize);
 
+		// Create background
 		const background = this.add.rectangle(
 			0, 0,
-			(gridSize.x * cellSize.x) + (gapSize.x * 0.5),
-			(gridSize.y * cellSize.y) + (gapSize.y * 0.5),
-			0x555fff
+			gridSize.x * cellSize.x + gapSize.x,
+			gridSize.y * cellSize.y + gapSize.y,
+			0x5088ff
 		);
 
-		let tiles: Array<GameObject> = [];
-		for (let i = 0; i < gridSize.x; i++)
-			for (let i = 0; i < gridSize.y; i++)
-				tiles.push(createTile(this, tileSize));
+		// Create tiles
+		let tiles: Rectangle[] = [];
+		for (let i = 0; i < gridSize.x * gridSize.y; i++) {
+			tiles.push(createTile(this, tileSize));
+		}
 
-		// FIXME!!!
-		// Can't center container
 		const tileContainer = this.add.container(0, 0, tiles);
-		tileContainer.x = -150 * 3 * 0.5;
-		tileContainer.y = -150 * 3 * 0.5;
+		gridAlignCenter(tiles, gridSize, cellSize);
 
-		Phaser.Actions.GridAlign(
-			tiles, {
-			width: gridSize.x,
-			height: gridSize.y,
-			cellWidth: cellSize.x,
-			cellHeight: cellSize.y,
-			position: Phaser.Display.Align.CENTER,
-		});
-
+		// Add GameObjects to world container
 		world.add([
 			background,
 			tileContainer
@@ -59,6 +49,31 @@ export default class DemoScene extends Phaser.Scene {
 	}
 }
 
-function createTile(scene: Scene, size: Vector2): GameObject {
+function createTile(scene: Scene, size: Vector2): Rectangle {
 	return scene.add.rectangle(0, 0, size.x, size.y, 0xffffff);
+}
+
+function gridAlignCenter(items: Rectangle[], gridSize: Vector2, cellSize: Vector2) {
+	const initial_x = -cellSize.x * gridSize.x / 2 + cellSize.x / 2;
+	const initial_y = -cellSize.y * gridSize.y / 2 + cellSize.y / 2;
+	let curPosX = initial_x;
+	let curPosY = initial_y;
+	
+	let index_x = 0;
+	let index_y = 0;
+
+	for (var item of items) {
+		item.setPosition(curPosX, curPosY);
+		if (index_x < gridSize.x - 1) {
+			index_x += 1;
+			curPosX += cellSize.x;
+			continue;
+		}
+		if (index_y < gridSize.y - 1) {
+			index_x = 0;
+			curPosX = initial_x;
+			index_y += 1;
+			curPosY += cellSize.y;
+		}
+	}
 }
